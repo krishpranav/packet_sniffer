c231069ea2a08cf31b22a934032fa6a25389ecc0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2021-04-01 16:54:50 +0530

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,31 +1,13 @@
 import scapy.all as scapy
 from scapy.layers import http
+import argparse
 
-print("TOOL IS CREATED BY KRISNA PRANAV")
-print("Github Link https://www.github.com/krishpranav")
-print("DO NOT FOREGT TO FOLLOW ME :)")
-
-
-def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
-
-
-def process_sniffed_packet():
-    if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + packet([http.HTTPRequest].Path
-        print("[+]HTTP REQUEST >> " + url.decode())
-
-        if packet.haslayer(scapy.Raw):
-            load = str(packet[scapy.Raw].load)
-        keywords = ["username", "user", "login", "password", "pass"]
-        for keyword in keywords:
-            if:
-        keyword in load:
-            print("\n\n\[+] Possible Username/Password > " + load)
-        break
-
-
-sniff("en1")
-
-
-
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", dest="interface", help="Interface of the network (ex: enO / ethO)")
+    options = parser.parse_args()
+    if not options.interface:
+        parser.error("[!] Please add an interface to proceed, --help for more informations.")
+    return options
+ 
+ 
